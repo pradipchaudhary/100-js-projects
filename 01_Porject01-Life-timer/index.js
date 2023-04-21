e09aba165c725e9ad7169866b4b5fe212f547179
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-21 17:07:46 +0545

    Set Date of Birth Hander

--- 01_Porject01-Life-timer/index.js
+++ 01_Porject01-Life-timer/index.js
@@ -5,6 +5,9 @@ const settingContent = document.getElementById("settingContent");
 const dobButton = document.getElementById("dobButton");
 const dobValue = document.getElementById("dobValue");
 
+const initEle = document.getElementById("initEle");
+const afterDobEle = document.getElementById("afterDobEle");
+
 const settingToggle = () => {
 	if (isOpen) {
 		settingContent.classList.remove("hide");
@@ -17,8 +20,23 @@ const settingToggle = () => {
 
 const setDobHandler = () => {
 	dateOfBirth = dobValue.value;
-	console.log(dateOfBirth);
+
+	if (dateOfBirth) {
+		initEle.classList.add("hide");
+		afterDobEle.classList.remove("hide");
+		console.log(initEle);
+		console.log(afterDobEle);
+		console.log("if set date : ", dateOfBirth);
+	} else {
+		afterDobEle.classList.add("hide");
+		initEle.classList.remove("hide");
+		console.log("hello");
+		console.log("if Not set date : ", dateOfBirth);
+	}
+	// console.log(dateOfBirth);
 };
+
+setDobHandler();
 
 settingIcon.addEventListener("click", settingToggle);
 dobButton.addEventListener("click", setDobHandler);
