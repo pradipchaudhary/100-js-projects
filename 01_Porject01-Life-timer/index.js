67a7913c569a55518edf4b02b9563e40cd39bdb4
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-22 18:21:20 +0545

    setDobHandler function error solved.

--- 01_Porject01-Life-timer/index.js
+++ 01_Porject01-Life-timer/index.js
@@ -3,11 +3,22 @@ let dateOfBirth;
 const settingIcon = document.getElementById("settingIcon");
 const settingContent = document.getElementById("settingContent");
 const dobButton = document.getElementById("dobButton");
-const dobValue = document.getElementById("dobValue");
+const dobInputValue = document.getElementById("dobValue");
+const dobV = dobInputValue.value;
+console.log("DOB Value is : ", dobV);
 
 const initEle = document.getElementById("initEle");
 const afterDobEle = document.getElementById("afterDobEle");
 
+// Select Days Elements
+const yearEl = document.getElementById("year");
+const monthEl = document.getElementById("month");
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+
+// Setting Toggles Function
 const settingToggle = () => {
 	if (isOpen) {
 		settingContent.classList.remove("hide");
@@ -18,25 +29,33 @@ const settingToggle = () => {
 	console.log(isOpen);
 };
 
+// Update Ages
+const updateAge = () => {
+	const currentDate = new Date();
+	const dateDifferent = currentDate - dateOfBirth;
+	// Year, month, day, hour , minute, second
+	const year = Math.floor(dateDifferent / (1000 * 60 * 60 * 24 * 365));
+
+	// set date in dom
+	yearEl.innerHTML = year;
+	console.log("Year Value : ", year);
+};
+
 const setDobHandler = () => {
-	dateOfBirth = dobValue.value;
+	const stringDate = dobInputValue.value;
+	dateOfBirth = stringDate ? new Date(stringDate) : null;
 
 	if (dateOfBirth) {
 		initEle.classList.add("hide");
 		afterDobEle.classList.remove("hide");
-		console.log(initEle);
-		console.log(afterDobEle);
-		console.log("if set date : ", dateOfBirth);
+		updateAge();
 	} else {
-		afterDobEle.classList.add("hide");
 		initEle.classList.remove("hide");
-		console.log("hello");
-		console.log("if Not set date : ", dateOfBirth);
+		afterDobEle.classList.add("hide");
 	}
-	// console.log(dateOfBirth);
 };
 
-setDobHandler();
+// setDobHandler();
 
 settingIcon.addEventListener("click", settingToggle);
 dobButton.addEventListener("click", setDobHandler);
