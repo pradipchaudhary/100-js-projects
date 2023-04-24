3d00a473764bf5e3fefb5968c04a79d209eb7026
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-25 04:56:12 +0545

    github profile project design complete.

--- 02_Github_Profile_Search/index.js
+++ 02_Github_Profile_Search/index.js
@@ -1,1 +1,9 @@
-const url = "";
+const url = "https://api.github.com/users/default";
+
+const fetchProfile = async () => {
+	const response = await fetch(url);
+	const data = await response.json();
+	console.log(data);
+};
+
+fetchProfile();
