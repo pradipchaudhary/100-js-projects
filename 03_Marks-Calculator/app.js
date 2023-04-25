6aa60209f6d7c399ec0c7bbae18c84a9d579b35f
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-25 18:08:09 +0545

    finish javascript project 3 (marks calculator).

--- 03_Marks-Calculator/app.js
+++ 03_Marks-Calculator/app.js
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+const calculatorFormEl = document.getElementById("calculatorForm");
+const englishEl = document.getElementById("english");
+const nepaliEl = document.getElementById("nepali");
+const mathEl = document.getElementById("math");
+const scienceEl = document.getElementById("science");
+const result = document.getElementById("result");
+
+const markCalculator = (event) => {
+	event.preventDefault();
+
+	const maxMarks = 400;
+
+	const english = parseInt(englishEl.value);
+	const nepali = parseInt(nepaliEl.value);
+	const math = parseInt(mathEl.value);
+	const science = parseInt(scienceEl.value);
+
+	console.log("Mark : ", english, nepali, math, science);
+	console.log("types of english numbers : ", english);
+
+	const totalMarks = english + nepali + math + science;
+	const percentage = Math.round((totalMarks / maxMarks) * 100);
+	console.log(totalMarks);
+	console.log(percentage);
+
+	result.innerText = `You have got ${totalMarks} marks out of ${maxMarks} and your percentage is ${percentage}%`;
+};
+
+// `You have got ${totalMarks} marks out of ${maxMarks} and your percentage is ${percentage}%`;
