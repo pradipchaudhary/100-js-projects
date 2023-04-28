dabe8edecf45574f919cb96a3e7512a5e1bb5f1d
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-28 20:39:02 +0545

    random number generate..

--- 04_Math_Quiz_App/app.js
+++ 04_Math_Quiz_App/app.js
@@ -1,5 +1,16 @@
+// Create a Math question
+// Math question will have a random generated
+// Question Type Multiplicatin question  with random number range 1-10
+// Answer will be the product of the random number range and the random number range
+// User will have to answer question
+// On submit  answer answer will be compared with random generated answer
+// If answer will be correct than score will be incremented
+// If answer will be wrong than score will be decremented
+
 const randomNumber = () => {
 	console.log("hello world!");
 };
 
 randomNumber();
+
+// return Math.floor(Math.random() * (max - min + 1) + min);
