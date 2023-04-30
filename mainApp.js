2a76d6b866385005820336bf6b581bc8db152474
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-30 14:41:17 +0545

    add project list dynamic ...

--- mainApp.js
+++ mainApp.js
@@ -1,1 +1,32 @@
-console.log("Welcome to JavaScript Project List");
+const list = document.querySelector(".list");
+
+fetch("./projects.json")
+	.then((res) => res.json())
+	.then((data) => updateUI(data));
+
+// Const UpdateUI
+const updateUI = (projects) => {
+	projects.map(({ name, code, index }) => {
+		// console.log(name);
+		const itemList = document.createElement("li");
+		itemList.innerHTML = `
+		<span class="project-number">${index}</span>
+		<a href="/${name}/index.html" class="project-name">
+		    ${projectNameFormatter(name)}
+		</a>
+		<a href="${code}" class="container-links">
+		    Code
+		</a>
+		`;
+		list.appendChild(itemList);
+	});
+};
+
+// Project Name Formatter
+
+const projectNameFormatter = (name) => {
+	return name
+		.split("-")
+		.map((word) => word[0].toUpperCase() + word.slice(1))
+		.join(" ");
+};
