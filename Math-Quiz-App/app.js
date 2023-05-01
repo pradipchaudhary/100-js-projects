4254d5c2d6d15c5895763383b8bcc9b710d2913d
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-01 20:43:52 +0545

    generate all types of question and store score in localStorage ...

--- Math-Quiz-App/app.js
+++ Math-Quiz-App/app.js
@@ -3,7 +3,7 @@ const scoreEl = document.getElementById("score");
 const answerEl = document.getElementById("answer");
 const wrongAnswerEl = document.getElementById("wrongAnswer");
 let storedAnswer;
-let score = 0;
+let score = localStorage.getItem("score");
 
 const randomNumber = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1) + min);
@@ -14,9 +14,39 @@ randomNumber();
 const generateQuestion = () => {
 	const randomNumber1 = randomNumber(1, 9);
 	const randomNumber2 = randomNumber(2, 9);
+	const questionType = randomNumber(1, 4);
+	let question;
+	let answer;
+	let firstNumber;
+	let secondNumber;
 
-	const question = `Q. What is ${randomNumber1} multiply by ${randomNumber2} ?`;
-	const answer = randomNumber1 * randomNumber2;
+	if (randomNumber1 > randomNumber2 && questionType > 2) {
+		firstNumber = randomNumber1;
+		secondNumber = randomNumber2;
+	} else {
+		firstNumber = randomNumber2;
+		secondNumber = randomNumber1;
+	}
+
+	switch (questionType) {
+		case 1:
+			question = `Q. What is ${firstNumber} multiply by ${secondNumber} ?`;
+			answer = firstNumber * secondNumber;
+			break;
+		case 2:
+			question = `Q. What is ${firstNumber} Add to ${secondNumber} ?`;
+			answer = firstNumber + secondNumber;
+			break;
+		case 3:
+			question = `Q. What is ${firstNumber} Divided by ${secondNumber} ?`;
+			answer = firstNumber / secondNumber;
+			break;
+		case 4:
+			question = `Q. What is ${firstNumber} Subtract from ${secondNumber} ?`;
+			answer = firstNumber - secondNumber;
+			break;
+	}
+
 	return { question, answer };
 };
 
@@ -24,6 +54,11 @@ const showQuestion = () => {
 	const { question, answer } = generateQuestion();
 	questionEl.innerText = question;
 	storedAnswer = answer;
+	if (score == null) {
+		scoreEl.innerText = 0;
+	} else {
+		scoreEl.innerText = score;
+	}
 };
 
 showQuestion();
@@ -34,15 +69,16 @@ const checkAnswer = (e) => {
 	let userAnswer = parseInt(answerEl.value);
 
 	if (storedAnswer == userAnswer) {
-		score = score + 1;
+		score++;
 		wrongAnswerEl.innerText = "";
 	} else {
-		score = score - 1;
+		score--;
 		wrongAnswerEl.innerText = "Wrong answer. Please try again !";
 		wrongAnswerEl.style.color = "red";
 	}
 
 	scoreEl.innerText = score;
+	localStorage.setItem("score", score);
 	e.target.reset();
 	showQuestion();
 	// answerEl.value = "";
