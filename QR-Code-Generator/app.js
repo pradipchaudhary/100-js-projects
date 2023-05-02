240e1c5b55104a1e42be653e8289b97481640748
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-02 15:03:25 +0545

    Finish QR Code generator....

--- QR-Code-Generator/app.js
+++ QR-Code-Generator/app.js
@@ -1,9 +1,39 @@
-const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example`;
+// const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example`;
 
 const qrGeneratorBtn = document.getElementById("qrGeneratorBtn");
 const qrForm = document.getElementById("qrForm");
+const qrCode = document.querySelector(".qrCode");
+
+// Render QR Code
+const renderQRCode = (url) => {
+	const imageTag = qrCode.getElementsByTagName("img");
+	const img = document.createElement("img");
+	img.setAttribute("src", url);
+	console.log("note image tag");
+	qrCode.appendChild(img);
+
+	// if (imageTag) {
+	// 	const img = document.createElement("img");
+	// 	img.setAttribute("src", url);
+	// 	console.log("have image Tage");
+	// } else {
+	// 	const img = document.createElement("img");
+	// 	img.setAttribute("src", url);
+	// 	console.log("note image tag");
+	// 	qrCode.appendChild(img);
+	// }
+};
+// renderQRCode(
+// 	"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example"
+// );
 
 qrForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-	console.log("QR Generator...");
+	console.log("QR Generator...", e);
+
+	const formData = new FormData(qrForm);
+	let qrText = formData.get("qrText");
+	let qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText}`;
+	renderQRCode(qrCodeUrl);
+	qrForm.reset();
 });
