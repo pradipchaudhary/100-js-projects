dbeacbce3744bbcb260f7b02977be671655ae46f
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-05 20:19:02 +0545

    update expense racker app layout file.

--- Expense-Tracker-App/app.js
+++ Expense-Tracker-App/app.js
@@ -1,1 +1,12 @@
-console.log("Welcome to the...");
+const transactionFormEl = document.getElementById("transactionForm");
+
+const addTransaction = (e) => {
+	e.preventDefault();
+	const getData = new FormData(transactionFormEl);
+	const getText = getData.get("text");
+	const getAmount = getData.get("amount");
+	console.log("Get Form Data: ", getText, getAmount);
+};
+
+// Event
+transactionFormEl.addEventListener("submit", addTransaction);
