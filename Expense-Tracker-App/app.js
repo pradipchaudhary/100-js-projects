e7218909cb5b66d4510354083a0b3078dfd3c2da
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-09 04:25:01 +0545

    Add transaction and render it.

--- Expense-Tracker-App/app.js
+++ Expense-Tracker-App/app.js
@@ -1,20 +1,67 @@
+// DOM Element
 const transactionFormEl = document.getElementById("transactionForm");
+const netAmountEl = document.getElementById("netAmount");
+const transactionsContainerEl = document.querySelector(".transactions");
+
+const initState = {
+	earnings: 0,
+	expenses: 0,
+	net: 0,
+	transaction: [
+		// {
+		// 	id: 001,
+		// 	text: "Demo transaction",
+		// 	amount: 100,
+		// 	type: "credit",
+		// },
+	],
+};
+
+const renderTransaction = () => {
+	const transaction = initState.transaction;
+
+	transaction.forEach((transaction) => {
+		const { id, text, amount, type } = transaction;
+		const isCredit = type === "credit" ? true : false;
+		const transactionEl = `
+			<div class="transaction">
+				<div class="left">
+					<p>${text} </p>
+					<p>+ Rs. ${amount}</p>
+				</div>
+				<div class="status ${isCredit ? "credit" : "debit"}">${
+			isCredit ? "C" : "D"
+		}</div>
+			</div>
+		`;
+		transactionsContainerEl.insertAdjacentHTML("afterbegin", transactionEl);
+	});
+
+	console.log("Hello world!");
+};
 
 const addTransaction = (e) => {
 	e.preventDefault();
+
+	const isEarn = e.submitter.id === "earnBtn" ? true : false;
+
 	const formData = new FormData(transactionFormEl);
 
 	const tData = {};
-	// for (const pair of formData.entries()) {
-	// 	console.log(pair[0] + ": " + pair[1]);
-	// 	console.log(pair);
-	// }
 	formData.forEach((value, key) => {
 		tData[key] = value;
 	});
-	console.log(tData);
-	console.log(e.submitter.id);
+	const { text, amount } = tData;
+	const transaction = {
+		id: Math.floor(Math.random() * 1000),
+		text: text,
+		amount: +amount,
+		type: isEarn ? "credit" : "debit",
+	};
+	initState.transaction.push(transaction);
+	console.log(initState);
+	renderTransaction();
 };
 
-// Event
+// Event Called
 transactionFormEl.addEventListener("submit", addTransaction);
