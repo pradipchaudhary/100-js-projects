7564f8a3f1ce1868f31298de12c35e6e943cd8bf
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-08 21:49:48 +0545

    get form data..

--- Expense-Tracker-App/app.js
+++ Expense-Tracker-App/app.js
@@ -2,10 +2,18 @@ const transactionFormEl = document.getElementById("transactionForm");
 
 const addTransaction = (e) => {
 	e.preventDefault();
-	const getData = new FormData(transactionFormEl);
-	const getText = getData.get("text");
-	const getAmount = getData.get("amount");
-	console.log("Get Form Data: ", getText, getAmount);
+	const formData = new FormData(transactionFormEl);
+
+	const tData = {};
+	// for (const pair of formData.entries()) {
+	// 	console.log(pair[0] + ": " + pair[1]);
+	// 	console.log(pair);
+	// }
+	formData.forEach((value, key) => {
+		tData[key] = value;
+	});
+	console.log(tData);
+	console.log(e.submitter.id);
 };
 
 // Event
