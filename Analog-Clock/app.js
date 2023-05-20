72171178eed082af719935d79a83d7d7991ff65e
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-20 07:57:27 +0545

    init click function ...

--- Analog-Clock/app.js
+++ Analog-Clock/app.js
@@ -1,1 +1,12 @@
-console.log("Analog Click");
+// DOM Elements
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+
+// Functions
+const initClock = () => {
+	console.log("ini Clock ⏲️");
+};
+
+// Event handlers
+initClock();
