2d4784637faca7beb8f5474536a7af33976915ce
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-27 21:14:49 +0545

    update..

--- Box-Shadow-Generator/app.js
+++ Box-Shadow-Generator/app.js
@@ -1,1 +1,14 @@
-console.log("connection");
+// DOM Element
+const boxShadows = document.querySelector(".boxShadow");
+const offsetX = document.querySelector(".offset-x");
+const offsetY = document.querySelector(".offset-y");
+const radius = document.querySelector(".blur-radius");
+const spread = document.querySelector(".spread-radius");
+const shadowColor = document.querySelector(".shadowColor");
+
+const updateBoxShadow = () => {
+    const v = `${offsetX.value}px ${offsetY.value}px ${radius.value}px ${spread.value}px ${shadowColor.value}`;
+    console.log(v);
+};
+
+updateBoxShadow();
