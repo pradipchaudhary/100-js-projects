04148f039a4ee971c7a6ba4642c6c0c87ed64ac2
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-28 04:30:40 +0545

    update box shadow value ...

--- Box-Shadow-Generator/app.js
+++ Box-Shadow-Generator/app.js
@@ -1,14 +1,30 @@
 // DOM Element
-const boxShadows = document.querySelector(".boxShadow");
+const boxShadowsEl = document.querySelector(".boxShadow");
 const offsetX = document.querySelector(".offset-x");
 const offsetY = document.querySelector(".offset-y");
 const radius = document.querySelector(".blur-radius");
 const spread = document.querySelector(".spread-radius");
 const shadowColor = document.querySelector(".shadowColor");
+const codeEl = document.querySelector(".code");
+// const boxShadowCode = `-webkit-box-shadow: 10px 10px 5px 0px
+//                         rgba(0,0,0,0.75); <br />
+//                         -moz-box-shadow: 10px 10px 5px 0px
+//                         rgba(0,0,0,0.75);<br />
+//                         box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);`;
+
+function updateCode() {
+    // code
+}
 
 const updateBoxShadow = () => {
     const v = `${offsetX.value}px ${offsetY.value}px ${radius.value}px ${spread.value}px ${shadowColor.value}`;
-    console.log(v);
+    const code = `box-shadow:${v}`;
+    codeEl.innerHTML = code;
+    console.log((boxShadowsEl.style.boxShadow = v));
+    boxShadowsEl.style.boxShadow = v;
+
+    // console.log(code);
 };
 
 updateBoxShadow();
+// console.log(codeEl);
