eb969acdc7e342dc0e0b21c6bce2c86382799c07
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-29 05:33:35 +0545

    finish box shadow generator app

--- Box-Shadow-Generator/app.js
+++ Box-Shadow-Generator/app.js
@@ -1,10 +1,12 @@
 // DOM Element
+const inputs = document.querySelectorAll("input");
 const boxShadowsEl = document.querySelector(".boxShadow");
 const offsetX = document.querySelector(".offset-x");
 const offsetY = document.querySelector(".offset-y");
 const radius = document.querySelector(".blur-radius");
 const spread = document.querySelector(".spread-radius");
 const shadowColor = document.querySelector(".shadowColor");
+const opacity = document.querySelector(".opacity");
 const codeEl = document.querySelector(".code");
 
 // Displays current value in DOM
@@ -14,24 +16,35 @@ const radiusValue = document.querySelector(".blur-radius-value");
 const spreadValue = document.querySelector(".spread-radius-value");
 const opacityValue = document.querySelector(".opacity-value");
 
-// const boxShadowCode = `-webkit-box-shadow: 10px 10px 5px 0px
-//                         rgba(0,0,0,0.75); <br />
-//                         -moz-box-shadow: 10px 10px 5px 0px
-//                         rgba(0,0,0,0.75);<br />
-//                         box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);`;
+// Hexadecimal to RGBA color
+const hexToRGBA = (hex) => {
+    let r = parseInt(hex.slice(1, 3), 16);
+    let g = parseInt(hex.slice(3, 5), 16);
+    let b = parseInt(hex.slice(5, 7), 16);
+    console.log(r, g, b);
+    return `rgba(${r}, ${g}, ${b}, ${opacity.value})`;
+};
 
 const updateBoxShadow = () => {
-    const v = `${offsetX.value}px ${offsetY.value}px ${radius.value}px ${spread.value}px ${shadowColor.value}`;
-    const code = `box-shadow: ${v}`;
-    codeEl.innerHTML = code;
-    boxShadowsEl.style.boxShadow = v;
     offsetXValue.innerHTML = `${offsetX.value} px`;
     offsetYValue.innerHTML = `${offsetY.value} px`;
     radiusValue.innerHTML = `${radius.value} px`;
     spreadValue.innerHTML = `${spread.value} px`;
+    opacityValue.innerHTML = `${opacity.value} px`;
+    const v = `${offsetX.value}px ${offsetY.value}px ${radius.value}px ${
+        spread.value
+    }px ${hexToRGBA(shadowColor.value)}`;
+    const code = `box-shadow: ${v}`;
+    codeEl.innerHTML = code;
+    boxShadowsEl.style.boxShadow = v;
 };
-[offsetX, offsetY, radius, spread, shadowColor].forEach((element) => {
+
+inputs.forEach((element) => {
     element.oninput = updateBoxShadow;
 });
+
 updateBoxShadow();
 // console.log(codeEl);
+console.log(shadowColor.value);
+
+hexToRGBA("#000000");
