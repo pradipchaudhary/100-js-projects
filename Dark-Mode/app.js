ee77cc4e83fa15e480668e8e14abbc7b38959820
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-31 22:00:29 +0545

    update dark mode app (100 javascript projects)

--- Dark-Mode/app.js
+++ Dark-Mode/app.js
@@ -1,15 +1,34 @@
-const toggle = document.getElementById("toggleDark");
+const toggle = document.getElementById("toggleButton");
 const body = document.querySelector("body");
 const title = document.querySelector(".title");
 
-toggle.addEventListener("click", function () {
-    this.classList.toggle("fa-moon");
-    body.classList.toggle("dark");
-    if (body.classList == "dark") {
-        title.innerHTML = "Click for light 🌄 mode.";
-        console.log("This is dark mood");
-    } else {
-        title.innerHTML = `Click for dark🌑 mode.`;
-        console.log("This is light mood");
+// toggle.addEventListener("click", function () {
+//     this.classList.toggle("fa-moon");
+//     // body.classList.toggle("dark");
+//     // if (body.classList == "dark") {
+//     //     title.innerHTML = "Click for light 🌄 mode.";
+//     //     console.log("This is dark mood");
+//     // } else {
+//     //     title.innerHTML = `Click for dark🌑 mode.`;
+//     //     console.log("This is light mood");
+//     // }
+// });
+
+const toogleButton = () => {
+    const imgById = document.getElementById("toggleButton");
+    if (imgById.src == "dark.png") {
+        document.getElementById("toggleButton").src = "light.png";
+        console.log("hello");
+    } else if (imgById.src == "light.png") {
+        document.getElementById("toggleButton").src = "dark.png";
+        console.log("Hi");
     }
-});
+};
+
+// const img = document.querySelector("img").src;
+// const imgById = document.getElementById("toggleButton").src;
+
+// console.log(img);
+// console.log(imgById);
+
+toggle.addEventListener("click", toogleButton);
