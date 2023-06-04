0b4e3303e71ebaa64dde9bcb8e596cd60de17f4b
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-06-05 02:00:48 +0545

    init Digital Clock (100 JavaScript Projects)

---
+++ Digital-Clock/index.js
@@ -0,0 +1,15 @@
+//Javascript for switch clock format
+
+//Get current time in javascript
+
+//Set the time period(AM/PM)
+
+//Set the 12-hour clock format
+
+//Add the 0 for the values lower than 10
+
+//Get the date in javascript
+
+//document.querySelector(".year").innerHTML = year;
+
+//Javascript for dot menu toggle
