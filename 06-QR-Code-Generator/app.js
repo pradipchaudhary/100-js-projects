13ffdc37d5e0c2ed3df54db2910f1bf5ceed9184
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-09-02 22:15:56 +0545

    fetch data ...

--- 06-QR-Code-Generator/app.js
+++ 06-QR-Code-Generator/app.js
@@ -1,1 +1,19 @@
 const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=default`;
+
+// DOM elements
+const inputText = document.getElementById("inputText");
+
+function generateQrCode(url) {
+    fetch(url)
+        .then((response) => {
+            console.log(response);
+            return response.text();
+        })
+        .then((qrCode) => {
+            const img = document.createElement("img");
+            console.log(qrCode);
+        })
+        .catch();
+}
+
+generateQrCode(qrCodeUrl);
