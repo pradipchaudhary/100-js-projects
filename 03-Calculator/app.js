4b0af18c3f7e82a2985b8589d47ab79757f8295a
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-11-09 18:33:56 +0545

    clean the file and init app .

--- 03-Calculator/app.js
+++ 03-Calculator/app.js
@@ -1,27 +1,6 @@
-const calculatorFormEl = document.getElementById("calculatorForm");
-const englishEl = document.getElementById("english");
-const nepaliEl = document.getElementById("nepali");
-const mathEl = document.getElementById("math");
-const scienceEl = document.getElementById("science");
-const result = document.getElementById("result");
-
-const markCalculator = (event) => {
-	event.preventDefault();
-
-	const maxMarks = 400;
-
-	const english = parseInt(englishEl.value);
-	const nepali = parseInt(nepaliEl.value);
-	const math = parseInt(mathEl.value);
-	const science = parseInt(scienceEl.value);
-
-	const totalMarks = english + nepali + math + science;
-	const percentage = Math.round((totalMarks / maxMarks) * 100);
-
-	result.innerText = `You have got ${totalMarks} marks out of ${maxMarks} and your percentage is ${percentage}%`;
+const initApp = () => {
+    console.log("init App");
+    alert("init App");
 };
 
-// convert string to number
-// const stringToNumber = +number;
-// console.log(stringToNumber);
-// console.log("type: ", typeof stringToNumber);
+initApp();
