d97c65c8b763d44a4ee33e81a8e3a206a95ae190
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-02-02 01:38:08 +0545

    basic image slider with next andd previous button

--- 07-Basic Image Slider/index.js
+++ 07-Basic Image Slider/index.js
@@ -1,5 +1,24 @@
-function initApp() {
-    console.log("init app");
+let currentIndex = 0;
+const slides = document.querySelector(".slider-wrapper").children;
+const totalSlides = slides.length;
+
+function changeSlide(direction) {
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    updateSlider();
 }
 
-initApp();
+function updateSlider() {
+    const newTransformValue = -currentIndex * 100 + "%";
+    document.querySelector(".slider-wrapper").style.transform =
+        "translateX(" + newTransformValue + ")";
+}
+
+// Optionally, you can add automatic sliding by uncommenting the line below:
+// setInterval(() => changeSlide(1), 3000);
