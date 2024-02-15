008655fd591f7f4f5afa3245a89d5fd74a00221e
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-02-15 18:05:01 +0545

    update...

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 "use strict";
-// const projectsItems = document.querySelector(".projects-items");
-const projectsItems = document.getElementById("projects-items");
+const list = document.querySelector(".projects-list");
+// const projectsItems = document.getElementById("projects-items");
 const canvas = document.getElementById("pradip-canvas"),
     context = canvas.getContext("2d");
 const ancherEl = document.querySelectorAll("a");
@@ -10,63 +10,29 @@ window.onload = function () {
     console.log("Loaded");
 };
 
-fetch("./api.json")
+fetch("./projects.json")
     .then((res) => res.json())
     .then((data) => updateUI(data));
 
 //  UpdateUI
-// const updateUI = (projects) => {
-//     projects.map(({ name, code, index }) => {
-//         const itemList = document.createElement("li");
-//         itemList.innerHTML = `
-// 		<span class="project-number">${index}</span>
-// 		<span class="project-name">
-//         <a href="/${index}-${name}/index.html" target="_blank" >
-// 		    ${projectNameFormatter(name)}
-// 		</a>
-//         </span>
-// 		<a href="${code}" target="_blank" class="code-link">
-// 		    ${"{"} code ${"}"}
-// 		</a>
-// 		`;
-//         list.appendChild(itemList);
-//     });
-// };
-
 const updateUI = (projects) => {
-    projects.map(
-        ({
-            id,
-            name,
-            category,
-            features,
-            problem_solve,
-            description,
-            tag,
-            language,
-            cover_img,
-            code_link,
-        }) => {
-            const projectItem = document.createElement("div");
-            projectItem.className = "project-item";
-            console.log(projectItem);
-            projectItem.innerHTML = `
-                            <img src="./assets/screen/thumbnail.png" alt="" />
-                            <div class="project-info">
-                                <div class="lang">${language}</div>
-                               <a href=${code_link} > 
-                               <h3 class="project-name">${name}</h3>
-                               </a>
-                                <p class="description">
-                                    ${description}
-                                </p>
-                                
-                            </div>
-        `;
-            projectsItems.appendChild(projectItem);
-        }
-    );
+    projects.map(({ name, code, index }) => {
+        const itemList = document.createElement("li");
+        itemList.innerHTML = `
+		<span class="project-number">${index}</span>
+		<span class="project-name">
+        <a href="/${index}-${name}/index.html" target="_blank" >
+		    ${projectNameFormatter(name)}
+		</a>
+        </span>
+		<a href="${code}" target="_blank" class="code-link">
+		    ${"{"} code ${"}"}
+		</a>
+		`;
+        list.appendChild(itemList);
+    });
 };
+
 // Project Name Formatter
 const projectNameFormatter = (name) => {
     return name
