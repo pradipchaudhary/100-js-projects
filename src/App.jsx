beeb502a1e6b0d8e22d84e8ba2ef213783d61ecc
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-10-19 22:17:27 +0545

    update logo and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import ProjectsPage from "./pages/ProjectsPage";
-import ProjectDetailPage from "./pages/ProjectDetailPage";
 
 const router = createBrowserRouter([
     {
@@ -13,9 +11,7 @@ const router = createBrowserRouter([
         element: <Layout />,
         children: [
             { path: "/", element: <HomePage /> }, // Home at root path
-            // { path: "about", element: <AboutPage /> },
-            // { path: "projects", element: <ProjectsPage /> },
-            // { path: "projects/:id", element: <ProjectDetailPage /> },
+            { path: "about", element: <AboutPage /> },
         ],
     },
 ]);
