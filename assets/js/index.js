dd4072dbd15d254ef37fbe3dea70f12ddfd0f94e
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-11-12 00:55:36 +0545

    Create project  section and Card UI

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
 console.log("connected js");
+
+const shortDescription = () => {
+    const description = document.querySelectorAll(".project-description");
+    console.log(description);
+};
+
+shortDescription();
