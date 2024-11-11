500034919f5f38706d5688c4c6d5ea9a85f78e9e
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-11-11 23:56:34 +0545

    update UI

---
+++ assets/js/demodata.js
@@ -0,0 +1,19 @@
+const project = {
+    index: "03",
+    name: "Digital Clock",
+    description: "A real-time digital clock that updates every second.",
+    difficulty: "Beginner",
+    techUsed: ["HTML", "CSS", "JavaScript"],
+    estimatedTime: "30 minutes",
+    status: "Completed",
+    startDate: new Date("2023-01-14T00:00:00.000Z"),
+    endDate: new Date("2023-01-14T00:00:00.000Z"),
+    thumbnail:
+        "https://raw.githubusercontent.com/pradipchaudhary/100-js-projects/refs/heads/project03/03-digital-clock/images/digital-clock-thumbnail.png",
+    codeLink:
+        "https://github.com/pradipchaudhary/100-js-projects/tree/project03/03-digital-clock",
+    liveDemoLink:
+        "https://pradipchaudhary.github.io/100-js-projects/digital-clock/",
+    tags: ["Clock", "JavaScript", "Beginner"],
+    createdAt: new Date("2023-01-14T00:00:00.000Z"),
+};
